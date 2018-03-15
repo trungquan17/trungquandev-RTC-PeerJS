a598ae6b2e46708466a19e91ff45d0145dad89a0
trungquan17 <quanlightning17@gmail.com>
2018-03-15 08:52:55 +0700

    second commit

--- main.js
+++ main.js
@@ -9,7 +9,42 @@ function playStream (idVideoTag, stream) {
     video.play;
 }
 
-openStream()
-    .then(stream => {
-        playStream('localStream', stream);
+//connect
+let peer = new Peer({ key: '2jqx54113lfh6w29' });
+peer.on('open', id => {
+    $('#my-peer').append(id);
+});
+
+//call
+$('#btnCall').bind('click', function () {
+    let id = $('#remoteId').val();
+
+    openStream()
+    .then(localStream => {
+        //play in local
+        playStream('localStream', localStream);
+
+        //start call and show video in remoteStream when user accept call
+        let call = peer.call(id, localStream);
+        call.on('stream', function (remoteStream) {
+            playStream('remoteStream', remoteStream);
+        });
     });
+});
+
+//answer
+peer.on('call', function (call) {
+    openStream()
+    .then(localStream => {
+        //answer stream
+        call.answer(localStream);
+        
+        //play in local
+        playStream('localStream', localStream);
+
+        //play in remote
+        call.on('stream', function (remoteStream) {
+            playStream('remoteStream', remoteStream);
+        });
+    });
+});
